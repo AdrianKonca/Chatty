4a836466ed20c229a9b7da43fe6fe5b156f04a90
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-06-07 23:00:19 +0200

    Initial commit;

---
+++ Data/ChattyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chatty.Data
+{
+    public class ChattyDbContext : IdentityDbContext
+    {
+        public ChattyDbContext(DbContextOptions<ChattyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
