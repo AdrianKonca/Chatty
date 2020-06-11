7a6d582591c86669a0ef7654955375fe3fe4c093
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-06-11 11:24:29 +0200

    Changes UserIdentity configuration to not required e-mail confirmation;

--- Startup.cs
+++ Startup.cs
@@ -31,11 +31,12 @@ namespace Chatty
             services.AddDbContext<ChattyDbContext>(options =>
                 options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<User>()
                 .AddRoles<IdentityRole<int>>()
                 .AddEntityFrameworkStores<ChattyDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
